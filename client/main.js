a3b7b6c8045cf7ebb72251cdc36da636a825adbe
Fernando Mumbach (Fermuch) <fermuch@cryptolab.net>
2016-05-21 23:59:44 -0300

    added type of `textarea`

--- client/main.js
+++ client/main.js
@@ -15,15 +15,13 @@ class PostsCreate extends React.Component {
   render() {
     return (
       <Form
-        state={this.state}
         logErrors={true}
         autoSave={true}
-        onChange={({ title, body }) => this.setState({ title, body })}
         onSubmit={(form) => console.info('onSubmit', form)}
       >
         {/* TextFieldComponent */}
         <Field fieldName="string" type="string" label="String" />
-        <Field fieldName="string" type="string" label="Disabled String" disabled={true} />
+        <Field fieldName="disabled_string" type="string" label="Disabled String" disabled={true} />
         <Field fieldName="text" type="text" label="Text" />
         <Field fieldName="number" type="number" label="Number" />
         <Field fieldName="date" type="date" label="Date" />
