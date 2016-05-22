0bda69b4b7b79447af543e3b1413fac8f65930e1
Fernando Mumbach (Fermuch) <fermuch@cryptolab.net>
2016-05-21 23:03:40 -0300

    initial commit

---
+++ client/main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Form, Field } from 'simple-react-form';
+import {Meteor} from 'meteor/meteor';
+
+import '../lib';
+
+class PostsCreate extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Form
+        state={this.state}
+        onChange={({ title, body }) => this.setState({ title, body })}
+      >
+        <Field fieldName="title" type="string" label="Title"/>
+      </Form>
+    );
+  }
+}
+
+Meteor.startup(() => {
+  ReactDOM.render(<PostsCreate />, document.getElementById('container'));
+});
